31137d26801c74aec435ca63aeed4071a18cf184
Taylor Gosselin <trgosselin14@gmail.com>
2018-03-26 20:45:57 -0400

    List of foods added, still to be groomed for correct DB items

--- nutritional_data.py
+++ nutritional_data.py
@@ -4,12 +4,15 @@ API to allow other modules to access the nutritional data.
 """
 
 import requests
+import pandas as pd
 
 api_key = "uccIJar8k7Ps1nLu9PCJprLAPVgQWaO6MUWtVYM1"
 
-#parameters = {"api_key":api_key, "lt":"g"}
+food_list = list(pd.read_csv(r'/home/trgosselin14/Projects/Optimal_Salad/food_list.csv').columns)
+
+
 food_ndbno = {"Kale":11233,}
-food_list = foods = ['Kale', 'Olives, pickled', 'Carrots', 'mozzarella cheese shredded', 'onion, raw', ]
+#food_list = foods = ['Kale', 'Olives, pickled', 'Carrots', 'mozzarella cheese shredded', 'onion, raw', ]
 # Return the NDBNO for food in a list
 def get_database_ids(list_of_food_names):
     dict_of_foods ={}
@@ -17,8 +20,6 @@ def get_database_ids(list_of_food_names):
         parameters = {"api_key":api_key,"q":food, 'ds':'Standard Reference',"format":'json'}
         search = requests.get(" https://api.nal.usda.gov/ndb/search/?", params=parameters)
         json_object = search.json()
-
-        try:
         try:
             dict_of_foods[json_object['list']['item'][0]['name']] = json_object['list']['item'][0]['ndbno']
         except:
